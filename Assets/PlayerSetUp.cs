bc1830c5fbb49dfce2805ce87cfc21c5aae1a3b3
Sam <mcloughlin.sam@gmail.com>
2016-02-12 16:26:48 +0000

    Added some materials

---
+++ Assets/PlayerSetUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerSetUp : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceenCam;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+
+        }
+        else
+        {
+            sceenCam = Camera.main;
+            if(sceenCam != null)
+            {
+                sceenCam.gameObject.SetActive(false);
+            }
+            
+        }
+
+
+    }
+	
+
+    void OnDisable()
+    {
+        if (sceenCam != null)
+        {
+            sceenCam.gameObject.SetActive(true);
+        }
+    }
+
+
+}
